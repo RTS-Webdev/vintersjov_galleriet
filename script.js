2ef4d4f3fd000eea4bfa289eb7ddfd5fceaf13c5
m <mikkelelvers@outlook.com>
2025-04-29 10:00:26 +0200

    feat: finished the project. it is so bad tho

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 function toggleMenu() {
-    const button = document.getElementById("dropdown");
-    button.classList.toggle("active");
+	const button = document.getElementById("dropdown");
+	button.classList.toggle("active");
+}
+
+function toggleDialog() {
+    const dialog = document.querySelector(".dialog");
+    dialog.classList.toggle("active");
 }
 
 const menu = document.getElementById("dropdown");
 menu.addEventListener("click", toggleMenu);
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+
+accordionHeaders.forEach((header) => {
+	header.addEventListener("click", function () {
+		const accordionItem = this.parentElement;
+
+		const allAccordionItems = document.querySelectorAll(
+			".faq__accordion__container article"
+		);
+		allAccordionItems.forEach((item) => {
+			if (item !== accordionItem && item.classList.contains("active")) {
+				item.classList.remove("active");
+			}
+		});
+
+		accordionItem.classList.toggle("active");
+	});
+});
+
+const firstAccordionItem = document.querySelector(
+	".faq__accordion__container article"
+);
+if (firstAccordionItem) {
+	firstAccordionItem.classList.add("active");
+}
